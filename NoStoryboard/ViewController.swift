dbbc0146edd4a17b756526c321b0ac64fd26d605
Eun Wonki <38225877+eunwonki@users.noreply.github.com>
2022-11-10 23:49:43 +0900

    Initial commit

---
+++ NoStoryboard/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NoStoryboard
+//
+//  Created by wonki on 2022/11/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .systemMint
+    }
+
+
+}
+
